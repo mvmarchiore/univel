b66cc35d439d533ec11ac8050dcde7b59f417fb1
Marcos V. Marchiore <161514489+mvmarchiore@users.noreply.github.com>
2025-10-29 21:15:04 -0300

    Add files via upload

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,10 +1,15 @@
 package model;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 public class Usuario {
   private int id;
   private String login;
   private String senha;
   private String dataCriacao;
+  private String tipo;
+  private String ativo;
 
   public int getId() { return id; }
   public void setId(int id) { this.id = id; }
@@ -15,6 +20,22 @@ public class Usuario {
   public String getSenha() { return senha; }
   public void setSenha(String senha) { this.senha = senha; }
 
+  public String getTipo() { return tipo; }
+  public void setTipo(String tipo) { this.tipo = tipo; }
+
+  public String getAtivo() { return ativo; }
+  public void setAtivo(String ativo) { this.ativo = ativo; }
+
   public String getDataCriacao(){ return dataCriacao; }
-  public void setDataCriacao(String dataCriacao) { this.dataCriacao = dataCriacao; }
+  public void setDataCriacao(LocalDateTime localDateTime) { 
+    throw new UnsupportedOperationException("Unimplemented method 'setDataCriacao'");
+  }
+  public void setDataCriacao(String string) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setDataCriacao'");
+  }
+  public void setDataCriacao(Timestamp timestamp) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setDataCriacao'");
+  }
 }
